38e0248aa40a8e98116a7e8450cfbbc759c39ba2
Sung-jin Park <75429815+Denev6@users.noreply.github.com>
2023-09-03 18:45:09 +0900

    update 13/11.py

--- 단계별로풀어보기/13.정렬/11.py
+++ 단계별로풀어보기/13.정렬/11.py
@@ -6,9 +6,6 @@ print = sys.stdout.write
 _ = input()
 ordered_numbers = list(map(int, input().split()))
 sorted_numbers = sorted(set(ordered_numbers))
-res = dict()
-
-for i, n in enumerate(sorted_numbers):
-  res[n] = i
+res = dict((n, i) for i, n in enumerate(sorted_numbers))
 
 print(" ".join(str(res[n]) for n in ordered_numbers))
